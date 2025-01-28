2a2de083dd293d91fc9ea55d04b05011ccf698c3
sarahbhattii <sarahsanchia@usf.edu>
2025-01-27 21:48:29 -0500

    Add files via upload

---
+++ Module3.R
@@ -0,0 +1,12 @@
+#Module 3 Data frame
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+
+ABC.political.poll.results <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS.political.poll.results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+data <- data.frame(Name, ABC.political.poll.results, CBS.political.poll.results)
+data
+
+
